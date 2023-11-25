b5f23c98ea315695249d0b0344b894c7a33c936f
salimovrashad <selimovresad33@gmail.com>
2023-11-25 19:30:57 +0400

    add link hover

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const fetchApi = ()=>{
 .then(res=>res.json())
 .then(data=>{
     let li = "";
-    data.slice(0,12).map(item=>{
-        li+=`<div class="card col-sm-6 col-lg-3 col-md-4" style="width: 20rem; background-color: #151f30;">
+    data.slice(0,15).map(item=>{
+        li+=`<div class="card" style="width: 16rem; background-color: #151f30;">
         <img src=${item.image.medium} class="card-img-top" alt="...">
         <div class="card-body text-light">
           <h5 class="card-title">${item.name}</h5>
-          <p class="card-text">${item.summary}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p>${item.genres}</p>
+          <button href="#" class="btn btn-outline-primary p-0 px-3">Details</button>
         </div>
         </div>`
     })
